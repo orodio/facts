83b25cd1f95bba6bdbdb623fabf85cd389765ccb
James Bjorkman <james@bjorkman.ca>
2015-10-12 12:31:06 +1100

    VERSION 0.1.1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-import _ from "lodash"
+import _isBoolean   from "lodash/lang/isBoolean"
+import _isArray     from "lodash/lang/isArray"
+import _isFunction  from "lodash/lang/isFunction"
+import _isNull      from "lodash/lang/isNull"
+import _isNumber    from "lodash/lang/isNumber"
+import _isObject    from "lodash/lang/isObject"
+import _isRegExp    from "lodash/lang/isRegExp"
+import _isString    from "lodash/lang/isString"
+import _isUndefined from "lodash/lang/isUndefined"
+import _map         from "lodash/collection/map"
+import _any         from "lodash/collection/any"
+import _all         from "lodash/collection/all"
+import _zipWith     from "lodash/array/zipWith"
 
 export const BOOLEAN   = Symbol("boolean");
 export const ARRAY     = Symbol("array");
@@ -18,15 +30,15 @@ function checker (func, symb) {
   }
 }
 
-export const isBoolean   = checker(_.isBoolean,   BOOLEAN);
-export const isArray     = checker(_.isArray,     ARRAY);
-export const isFunction  = checker(_.isFunction,  FUNCTION);
-export const isNull      = checker(_.isNull,      NULL);
-export const isNumber    = checker(_.isNumber,    NUMBER);
-export const isObject    = checker(_.isObject,    OBJECT);
-export const isRegExp    = checker(_.isRegExp,    REGEXP);
-export const isString    = checker(_.isString,    STRING);
-export const isUndefined = checker(_.isUndefined, UNDEFINED);
+export const isBoolean   = checker(_isBoolean,   BOOLEAN);
+export const isArray     = checker(_isArray,     ARRAY);
+export const isFunction  = checker(_isFunction,  FUNCTION);
+export const isNull      = checker(_isNull,      NULL);
+export const isNumber    = checker(_isNumber,    NUMBER);
+export const isObject    = checker(_isObject,    OBJECT);
+export const isRegExp    = checker(_isRegExp,    REGEXP);
+export const isString    = checker(_isString,    STRING);
+export const isUndefined = checker(_isUndefined, UNDEFINED);
 
 function isLiteral (v) {
   switch (v) {
@@ -73,7 +85,7 @@ export function whatType (v) {
 }
 
 function any (pred, ...shapes) {
-  return _.any([for (s of shapes) pred(s)]);
+  return _any([for (s of shapes) pred(s)]);
 }
 
 function shape_literal(a, b) {
@@ -84,13 +96,13 @@ function shape_literal(a, b) {
 function shape_array (a, b) {
   if (!isArray(a) || !isArray(b)) return false;
   if (a.length === 0 && a.length === 0) return true;
-  return _.all(_.zipWith(a, b, shape))
+  return _all(_zipWith(a, b, shape))
 }
 
 function shape_object (a, b) {
   if (!isObject(a) || !isObject(b)) return false;
   if (Object.keys(a).length !== Object.keys(b).length) return false;
-  return _.all(_.map(a, (value, key) => {
+  return _all(_map(a, (value, key) => {
     if (b[key] == null) return false;
     return shape(a[key], b[key])
   }))
